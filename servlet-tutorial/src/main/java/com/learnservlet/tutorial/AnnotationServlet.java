8fc8dfbfdbdcc32846fcf200d63163e5d40bc139
lukang-local <lukang-local@example.com>
2021-11-06 18:05:21 +0800

    AnnotationServlet

---
+++ servlet-tutorial/src/main/java/com/learnservlet/tutorial/AnnotationServlet.java
@@ -0,0 +1,20 @@
+package com.learnservlet.tutorial;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * WebServlet 注解
+ * 无须在web.xml文件书写servlet的配置信息
+ */
+@WebServlet("/testannotation")
+public class AnnotationServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter pw=res.getWriter();
+        pw.println("how to use annotation");
+    }
+}
